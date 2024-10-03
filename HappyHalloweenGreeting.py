710829deec29ce9a4ec6f5562c6467fe6ec17f39
aish-017 <aisha_rkhan@hotmail.com>
2024-10-04 08:08:49 +1300

    Update HappyHalloweenGreeting.py
    I created a Halloween greeting. I want to add a few more features to this next time.

--- HappyHalloweenGreeting.py
+++ HappyHalloweenGreeting.py
@@ -1,9 +1,10 @@
-import time
+import time # I almost forgot to do this
 
 def print_halloween_greeting():
-    while True:
+    while True: # Display spooky message 
         print("Happy Halloween")
-        time.sleep(1)  # Pause for 1 second
+        time.sleep(1)  # Pause for 1 second 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     print_halloween_greeting()
+#Yayyyy! this code actually works
