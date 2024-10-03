195a970f3521e143b598881cfa1e4484cde8ecc5
aish-017 <aisha_rkhan@hotmail.com>
2024-10-04 08:04:08 +1300

    Add files via upload

---
+++ HappyHalloweenGreeting.py
@@ -0,0 +1,9 @@
+import time
+
+def print_halloween_greeting():
+    while True:
+        print("Happy Halloween")
+        time.sleep(1)  # Pause for 1 second
+
+if __name__ == "__main__":
+    print_halloween_greeting()
