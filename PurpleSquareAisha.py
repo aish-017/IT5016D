c8294b0c3f4a19c71a2ebd42dff09f29bf7ec99d
aish-017 <aisha_rkhan@hotmail.com>
2024-10-04 09:03:47 +1300

    Update PurpleSquareAisha.py
    Attempted to make a purple square with my name in the centre again.

--- PurpleSquareAisha.py
+++ PurpleSquareAisha.py
@@ -7,29 +7,29 @@ t.speed("fastest")  # Set the drawing speed to the fastest
 # Define the side length of the square
 side_length = 200
 
-# Set the fill color to purple
-t.fillcolor("purple")
-t.begin_fill()
+# Set the fill colour to purple
+t.fillcolor("purple") # Prepares to fill in square purple
+t.begin_fill() 
 
 # Draw the square
-for _ in range(4):
-    t.forward(side_length)
-    t.right(90)
+for _ in range(4): # Loop draws 4 sides of square
+    t.forward(side_length) # moves turtle forward
+    t.right(90) # Turns turtle 90 degrees for each side 
 
 t.end_fill()
 
 # Move to the center for the name
 t.penup()
-t.goto(0, 0)  # Go to the center of the square
+t.goto(0, 0)  # Go to the centre of the square
 t.pendown()
 
 # Write the name "Aisha" in black
 t.color("black")
-t.write("Aisha", align="center", font=("Arial", 16, "bold"))
+t.write("Aisha", align="centre", font=("Arial", 16, "bold")) # Displays Aisha in black, however I still cannot get my name to align to the centre 
 
 # Hide the turtle cursor
 t.hideturtle()
 
 # Keep the window open until manually closed
-turtle.done()
+turtle.done() # We have a purple square, yet still unsuccessful in getting my name in the centre
 
